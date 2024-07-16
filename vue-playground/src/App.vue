3743b7d611efd92f5af05dcf818a7008b746299d
Boris Barac <boris@Boriss-MacBook-Pro.local>
2024-07-16 14:38:45 +0200

    init done i hope

--- vue-playground/src/App.vue
+++ vue-playground/src/App.vue
@@ -1,47 +1,29 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+<template class="template">
+  <div class="template container">
+    <div style="background-color: aqua" class="item">1111</div>
+    <div style="background-color: yellow" class="item">22222</div>
+    <div style="background-color: orange" class="item">333333</div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.template {
+  width: 100%;
+  height: 100%;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: stretch;
+  align-content: center;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.item {
+  flex-grow: 1;
+  flex-basis: auto;
+  align-self: auto;
 }
 </style>
