aa4e9a41b017ff80f2fd58b32316e0f47f7fb705
Boris Barac <boris@Boriss-MacBook-Pro.local>
2024-07-16 18:30:27 +0200

    Slide components prepared

--- vue-playground/src/App.vue
+++ vue-playground/src/App.vue
@@ -1,29 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PannelContainer from './components/playground/PannelContainer.vue'
+</script>
 
 <template class="template">
-  <div class="template container">
-    <div style="background-color: aqua" class="item">1111</div>
-    <div style="background-color: yellow" class="item">22222</div>
-    <div style="background-color: orange" class="item">333333</div>
-  </div>
+  <PannelContainer />
 </template>
 
 <style scoped>
 .template {
-  width: 100%;
+  /* width: 100%; */
   height: 100%;
-}
-
-.container {
-  display: flex;
-  justify-content: center;
-  align-items: stretch;
-  align-content: center;
-}
-
-.item {
-  flex-grow: 1;
-  flex-basis: auto;
-  align-self: auto;
 }
 </style>
---
+++ vue-playground/src/components/playground/PannelContainer.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import Split from 'split.js'
+
+onMounted(() => {
+  Split(['#panel-1', '#panel-2', '#panel-3'])
+})
+</script>
+
+<template>
+  <div class="template container">
+    <div id="panel-1" style="background-color: aqua" class="item"></div>
+    <div id="panel-2" style="background-color: yellow" class="item"></div>
+    <div id="panel-3" style="background-color: orange" class="item"></div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: stretch;
+  align-content: center;
+}
+
+.item {
+  margin: 0.5rem;
+  flex-grow: 1;
+  flex-basis: auto;
+  align-self: auto;
+}
+
+.split {
+  display: flex;
+  flex-direction: row;
+}
+</style>
