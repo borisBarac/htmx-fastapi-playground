1ba1fdfd0cc8f168b3d99c751033117cb18d316a
Boris Barac <boris@Boriss-MacBook-Pro.local>
2024-07-17 21:30:59 +0200

    iframe prepared

---
+++ vue-playground/src/components/playground/HtmxRenderer.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { inject, watch } from 'vue'
+
+interface Props {
+  urlEventKey: string
+}
+const props = defineProps<Props>()
+const iFrameUrl: string = inject(props.urlEventKey)
+
+watch(shouldReload, () => {
+  iframe.src = { iFrameUrl }
+})
+</script>
+
+<template>
+  <div>
+    <h2>My Iframe</h2>
+    <iframe frameborder="0" scrolling="yes"></iframe>
+  </div>
+</template>
+
+<style scoped>
+/* Add any styles for the iframe container here */
+iframe {
+  width: 100%;
+  height: 400px; /* Adjust height as needed */
+}
+</style>
--- vue-playground/src/consts.ts
+++ vue-playground/src/consts.ts
@@ -1,2 +1,6 @@
 export type MonacoThemesEnum = 'vs-dark' | 'vs'
 export type CodeLanguages = 'json' | 'html'
+
+export enum ProviderEvents {
+  Reload = 'ProviderEventsReloadEvent'
+}
