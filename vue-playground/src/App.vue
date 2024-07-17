82d084673e28eac7ab8d8bd47dbb29f190f06e1e
Boris Barac <boris@Boriss-MacBook-Pro.local>
2024-07-17 22:40:36 +0200

    iframe bind working
    ...

--- vue-playground/src/App.vue
+++ vue-playground/src/App.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
-import CodeEditor from './components/playground/CodeEditor/CodeEditor.vue'
+import HtmxRenderer from './components/playground/HtmxRenderer.vue'
+import { ProviderEvents } from './consts'
+
+import { ref, provide } from 'vue'
+
+// by providing a ref, the GrandChild
+// can react to changes happening here.
+const bind_val = ref('www.google.com')
+provide(ProviderEvents.Reload, bind_val)
 </script>
 
 <template class="template">
-  <CodeEditor class="code-editor" language="json" editorId="1" />
+  <input v-model="bind_val" />
+
+  <HtmxRenderer
+    class="code-editor"
+    :urlEventKey="ProviderEvents.Reload"
+    :minSecDebounce="1"
+    :maxSecDebounce="5"
+  />
 </template>
 
 <style scoped>
@@ -17,3 +32,5 @@ import CodeEditor from './components/playground/CodeEditor/CodeEditor.vue'
   height: 100%;
 }
 </style>
+
+<script setup></script>
--- vue-playground/src/components/playground/HtmxRenderer.vue
+++ vue-playground/src/components/playground/HtmxRenderer.vue
@@ -1,28 +1,39 @@
 <script setup lang="ts">
-import { inject, watch } from 'vue'
+import { inject, ref } from 'vue'
+import { watchDebounced } from '@vueuse/core'
 
 interface Props {
   urlEventKey: string
+  minSecDebounce: number
+  maxSecDebounce: number
 }
 const props = defineProps<Props>()
-const iFrameUrl: string = inject(props.urlEventKey)
+const iFrameUrl: any = inject(props.urlEventKey)
 
-watch(shouldReload, () => {
-  iframe.src = { iFrameUrl }
-})
+var deboundedUrl = ref('')
+var reloadCount = ref(0)
+
+watchDebounced(
+  iFrameUrl,
+  () => {
+    reloadCount.value = reloadCount.value + 1
+    deboundedUrl.value = iFrameUrl.value
+  },
+  { debounce: props.minSecDebounce * 1000, maxWait: props.maxSecDebounce * 1000 }
+)
 </script>
 
 <template>
-  <div>
-    <h2>My Iframe</h2>
-    <iframe frameborder="0" scrolling="yes"></iframe>
+  <div class="fit-w-h">
+    <!-- <h2>{{ reloadCount }}</h2>
+    <h2>{{ deboundedUrl }}</h2> -->
+    <iframe class="fit-w-h" :src="deboundedUrl" frameborder="0" scrolling="yes" />
   </div>
 </template>
 
 <style scoped>
-/* Add any styles for the iframe container here */
-iframe {
+.fit-w-h {
   width: 100%;
-  height: 400px; /* Adjust height as needed */
+  height: 100%;
 }
 </style>
